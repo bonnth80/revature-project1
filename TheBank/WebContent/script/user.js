b79702c40d6baeebe73c60a083fe77581ab74f35
Tom Bonner <zerosavant@yahoo.com>
2020-01-12 22:11:39 -0600

    added stubs for account details function

--- TheBank/WebContent/script/user.js
+++ TheBank/WebContent/script/user.js
@@ -86,6 +86,8 @@ function getTransfers() {
       ]
    };
 }
+
+
 // ************* Accounts **************************
 var scAccounts = document.getElementById("scAccounts");
 
@@ -106,7 +108,7 @@ function populateAccounts() {
    accounts.forEach((element)=>{
       accountsTable +=
       '<tr>' +
-      '<td><button accountId="' + element.accountNumber + '" type="button" class="btn btn-primary">Details</button></td>' +
+      '<td><button onclick="displayAccountTransactionHistory(' + element.accountNumber + ')" type="button" class="btn btn-primary">Details</button></td>' +
       '<td>' + element.accountNumber + '</td>' +
       '<td>' + element.availableBalance + '</td>' +
       '</tr>'
@@ -115,6 +117,7 @@ function populateAccounts() {
    accountsTable += `</tbody></table></div>`;
 
    scAccounts.innerHTML = accountsTable;
+
 
 }
 
@@ -143,3 +146,134 @@ function getAccounts() {
 }
 
 populateAccounts();
+
+
+// ************* Transactions **************************
+var snAccountDetails = document.getElementById("snAccountDetails");
+
+function displayAccountTransactionHistory(accountNumber) {
+
+   let transactionObject = getTransactionHistory(accountNumber);
+   let transactionList = transactionObject.transactions;
+   let balance = Number.parseFloat(transactionObject.currentBalance);
+   let credit;
+   let debit;
+
+   transactionList = sortTL(transactionList);
+
+   let accountDetailsTable = `<h4 class="nav-item">Account details for: ${accountNumber}</h4>
+   <nav class="my-2">
+      <button type="button" class="btn btn-primary nav-item">Deposit</button>
+      <button type="button" class="btn btn-primary nav-item">Withdraw</button>
+      <button type="button" class="btn btn-primary nav-item">Transfer</button>
+   </nav>
+   <h5>Transaction History</h5>
+   <table class="table">
+      <thead>
+         <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Acting Party</th>
+            <th scope="col">Credit</th>
+            <th scope="col">Debit</th>
+            <th scope="col">Balance</th>
+         </tr>
+      </thead>
+      <tbody>`;
+
+   transactionList.forEach((element) => {
+      credit = Number.parseFloat(element.credit);
+      debit = Number.parseFloat(element.debit);
+      accountDetailsTable +=
+      '<tr>' +
+      '<td>' + element.transactionDate + '</td>' +
+      '<td>' + element.actingParty.toUpperCase() + '</td>' +
+      '<td style="color: green">' + (credit ? credit.toFixed(2) : '') + '</td>' +
+      '<td style="color: red">' + (debit ? debit.toFixed(2) : '') + '</td>' +
+      '<td>' + balance.toFixed(2) + '</td>' +
+      '</tr>';
+
+      // odd though this may seem since we're going in reverse date order
+      // we must calculate balance backwards
+      if (credit) {
+         balance -= credit;
+      } else if (debit) {
+         balance += debit;
+      }
+   })
+
+
+   snAccountDetails.innerHTML = accountDetailsTable;
+}
+
+function getTransactionHistory(accountNumber) {
+   // TODO: replace temp stub getAccounts
+   return {
+      transactions: [
+         {
+            transactionId: "32",
+            accountId: accountNumber,
+            actingParty: "merle's bbq",
+            credit: "",
+            debit: "14.25",
+            transactionDate: "10/02/2019",
+            transferId: ""
+         },
+         {
+            transactionId: "42",
+            accountId: accountNumber,
+            actingParty: "WITHDRAWAL",
+            credit: "",
+            debit: "100.00",
+            transactionDate: "10/01/2019",
+            transferId: ""
+         },
+         {
+            transactionId: "55",
+            accountId: accountNumber,
+            actingParty: "Maker mill",
+            credit: "215.51",
+            debit: "",
+            transactionDate: "10/06/2019",
+            transferId: ""
+         },
+         {
+            transactionId: "61",
+            accountId: accountNumber,
+            actingParty: "DEPOSIT",
+            credit: "100.00",
+            debit: "",
+            transactionDate: "10/013/2019",
+            transferId: ""
+         },
+         {
+            transactionId: "69",
+            accountId: accountNumber,
+            actingParty: "Nerd's vending, llc",
+            credit: "",
+            debit: "2.50",
+            transactionDate: "10/11/2019",
+            transferId: ""
+         }
+      ],
+      currentBalance: "425.00"
+   }
+}
+
+function sortTL(tl) {
+   // bubble sort tl
+   var newTl = tl;
+   var swapFlag = true;
+   while (swapFlag) {
+      swapFlag = false;
+      for (let i = 0; i < newTl.length - 1; i++) {
+         if (new Date(newTl[i].transactionDate) < new Date(newTl[i+1].transactionDate)) {
+            let temp = newTl[i];
+            newTl[i] = newTl[i+1];
+            newTl[i+1] = temp;
+            swapFlag = true;
+         }
+      }
+   }
+
+   return newTl;
+}
